9fe152de9425ff2c2fa3cebabb9a90782a1389c5
Toby Brain <tobio85@gmail.com>
2016-01-18 17:16:23 -0800

    Update collector to support mutiple series

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ var EventEmitter = require('events').EventEmitter;
 var influx = require('influx');
 var url = require('url');
 
-// create a collector for the given series
-function Collector(series, uri) {
-    if (!(this instanceof Collector)) {
-        return new Collector(series, uri);
+// create a collector for all series
+function Collector(uri) {
+    if (!(this instanceof Reporter)) {
+        return new Reporter(uri);
     }
 
     var self = this;
 
     if (!uri) {
         return;
-    }
-
-    if (!series) {
-        throw new Error('series name must be specified');
     }
 
     var parsed = url.parse(uri, true /* parse query args */);
@@ -36,13 +32,12 @@ function Collector(series, uri) {
         username : username,
         password : password,
         database : parsed.pathname.slice(1) // remove leading '/'
-    })
+    });
 
-    self._points = [];
+    self._series = {};
 
     var opt = parsed.query || {};
 
-    self._series_name = series;
     self._instant_flush = opt.instantFlush == 'yes';
     self._time_precision = opt.time_precision;
 
@@ -55,53 +50,76 @@ function Collector(series, uri) {
 
     // flush on an interval
     // or option to auto_flush=false
-    setInterval(function() {
+    self._interval = setInterval(function() {
         self.flush();
     }, flush_interval).unref();
 }
 
 Collector.prototype.__proto__ = EventEmitter.prototype;
 
-Collector.prototype.flush = function() {
-    var self = this;
-
-    if (!self._points || self._points.length === 0) {
+function flushSeries(collector, seriesName, points) {
+    if (!points || points.length === 0) {
         return;
     }
 
     // only send N points at a time to avoid making requests too large
-    // TODO what if we are backed up?
-    var points = self._points.splice(0, 50);
-    var opt = { precision: self._time_precision };
+    var batch = points.splice(0, 500);
+    var opt = { precision: collector._time_precision };
 
-    self._client.writePoints(self._series_name, points, opt, function(err) {
+    collector._client.writePoints(series._name, batch, opt, function(err) {
         if (err) {
-            // TODO if error put points back to send again?
-            return self.emit('error', err);
+            return collector.emit('error', err);
         }
 
         // there are more points to flush out
-        if (self._points.length >0) {
-            setImmediate(self.flush.bind(self));
+        if (points.length > 0) {
+            flushSeries(collector, seriesName, points);
         }
     });
+}
+
+Collector.prototype.flush = function() {
+    var self = this;
+
+    Object.keys(self._series).forEach(function(key) {
+        var series = self._series[key];
+        delete self._series[key];
+
+        flushSeries(self, key, series);
+    });
 };
+
+function getSeries(collector, name, reset) {
+    var series = collector._series[name];
+
+    if(!series) {
+        series = [];
+        collector._series[name] = series;
+    }
+
+    return series;
+}
 
 // collect a data point (or object)
 // @param [Object] value the data
 // @param [Object] tags the tags (optional)
-Collector.prototype.collect = function(value, tags) {
+Collector.prototype.collect = function(seriesName, value, tags) {
     var self = this;
 
-    // disabled (due to no URL)
-    if (!self._points) {
-        return;
-    }
-
-    self._points.push([value, tags]);
     if (self._instant_flush) {
-        self.flush();
+        flushSeries(self, seriesName, [[value, tags]]);
+    } else {
+        var series = getSeries(self, seriesName);
+
+        series.push([value, tags]);
     }
 };
+
+Collector.prototype.stop = function() {
+    var self = this;
+
+    clearInterval(self._interval);
+    self.flush();
+}
 
 module.exports = Collector;
